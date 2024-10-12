ddd2ffc0f886e3c92a0f959bb67909d63bfff989
IsmaBamo <missisma6@gmail.com>
2024-10-12 20:06:46 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 const corsOptions = {
     // origin:'http://localhost:5173',
-    origin:'https://taskify-xhf3.onrender.com',
+    origin:'https://taskify-xhf3.onrender.com/',
     credentials:true
 }
 
