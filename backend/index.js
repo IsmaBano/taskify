1c7460c886d0e44f91a41fc3d5bd04553b84bec0
IsmaBamo <missisma6@gmail.com>
2024-10-12 19:55:30 +0530

    deployment change

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,8 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin:'http://localhost:5173',
+    // origin:'http://localhost:5173',
+    origin:'https://taskify-xhf3.onrender.com',
     credentials:true
 }
 
