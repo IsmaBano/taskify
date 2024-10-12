cf30364222aea6aa04c61f3ba72f1894342769de
IsmaBamo <missisma6@gmail.com>
2024-10-12 20:39:53 +0530

    deploy change

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT="http://localhost:3000/api/v1/user";
-export const SOCIAL_API_END_POINT="http://localhost:3000/api/v1/social";
+export const USER_API_END_POINT="https://taskify-xhf3.onrender.com/api/v1/user";
+export const SOCIAL_API_END_POINT="https://taskify-xhf3.onrender.com/api/v1/social";
